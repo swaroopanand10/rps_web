ee92b153aa24573fef50dba510be714548efe452
swaroopanand <swaroopanand695@gmail.com>
2023-03-17 22:29:02 +0530

    removed all the console.logs, working fine

--- index.js
+++ index.js
@@ -24,28 +24,21 @@ function btnClicked(e) {
   count++
   game()
   if (rounds.value == count) {
-    console.log('game ends')
     appendResultsh1('game ends; Final results')
     // NOW SHOWING THE FINAL REUSULTS
     // prinnting the number of times user and comp won in the whole game out of all rounds
-    console.log(`user won ${user} rounds`)
     appendResults(`user won ${user} rounds`)
 
-    console.log(`computer won ${comp} rounds`)
     appendResults(`computer won ${comp} rounds`)
 
-    console.log(`${rounds.value - user - comp} rounds are tied`)
     appendResults(`${rounds.value - user - comp} rounds are tied`)
 
     if (user > comp) {
       // printing the final reuslt accordint to the values of user and comp
-      console.log('user wins the  whole game')
       appendResults('user wins the  whole game')
     } else if (user < comp) {
-      console.log('computer wins the whole game')
       appendResults('computer wins the whole game')
     } else {
-      console.log('game tied')
       appendResults('game tied')
     }
     choiceMenu.style.visibility = 'hidden'
@@ -57,18 +50,15 @@ function btnClicked(e) {
 function round(computerChoice, userChoice) {
   //this function evaluates the winner for one round according to the values of human and computer
   if (computerChoice == userChoice) {
-    console.log('tied')
     appendResults('tied')
     return
   }
 
   if (computerChoice == 'rock') {
     if (userChoice == 'paper') {
-      console.log('user wins, paper beats rock')
       appendResults('user wins, paper beats rock')
       user++
     } else {
-      console.log('computer wins, rock beats scissor')
       appendResults('computer wins, rock beats scissor')
       comp++
     }
@@ -76,11 +66,9 @@ function round(computerChoice, userChoice) {
 
   if (computerChoice == 'paper') {
     if (userChoice == 'rock') {
-      console.log('computer wins, paper beats rock')
       appendResults('computer wins, paper beats rock')
       comp++
     } else {
-      console.log(' user wins, scissor beats paper')
       appendResults(' user wins, scissor beats paper')
       user++
     }
@@ -88,11 +76,9 @@ function round(computerChoice, userChoice) {
 
   if (computerChoice == 'scissor') {
     if (userChoice == 'paper') {
-      console.log('computer wins, scissor beats paper')
       appendResults('computer wins, scissor beats paper')
       comp++
     } else {
-      console.log('user wins, rock beats scissor')
       appendResults('user wins, rock beats scissor')
       user++
     }
@@ -124,10 +110,8 @@ function game() {
   // let userChoice = prompt('enter your choice').toLowerCase() // asking user for a choice
   appendResultsh1(`Round ${count} Starts`)
   let a = `computer choice is ${computerChoice}`
-  console.log(a)
   appendResults(a)
   a = `user choice is ${userChoice}`
-  console.log(a)
   appendResults(a)
 
   if (choices.indexOf(userChoice) == -1) {
@@ -135,9 +119,7 @@ function game() {
     throw new Error('please enter a valid choice')
   }
 
-  console.log(`round results: `)
   appendResultsh1(`round ${count} results: `)
   round(computerChoice, userChoice) // Prinnting the results for each round
-  console.log('\n')
 
 }
